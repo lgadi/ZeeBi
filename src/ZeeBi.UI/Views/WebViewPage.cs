c3605d8c69fdc7efc7e69398f36bbc9119385132
Gadi Lifshitz <gadilif@gmail.com>
2011-02-23 18:31:14 +0200

    add QRCode image to Info.  Added @FullRoot to base class

---
+++ src/ZeeBi.UI/Views/WebViewPage.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ZeeBi.UI.Views
+{
+	public abstract class WebViewPage<T> : System.Web.Mvc.WebViewPage<T>
+	{
+		public static string FullRoot
+		{
+			get { return "http://" + HttpContext.Current.Request.Url.Authority; }
+		}
+	}
+}

ef3e7e9b15df1390dd391e61184022e50b3d73b9
Gadi Lifshitz <gadilif@gmail.com>
2011-02-23 16:33:03 +0200

    adding pageview count

---
+++ src/ZeeBi.UI/ViewModels/Stats/StatsViewModel.cs
@@ -0,0 +1,12 @@
+using ZeeBi.UI.Models;
+
+namespace ZeeBi.UI.ViewModels.Stats
+{
+	public class StatsViewModel
+	{
+		public Url Url { get; set; }
+		public int PageViewCount { get; set; }
+
+
+	}
+}

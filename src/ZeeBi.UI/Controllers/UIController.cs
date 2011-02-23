4fcd7ea17b866c3c516111fe7c31c3a1062aab4f
Avish <avishay.lavie@gmail.com>
2011-02-23 10:56:34 +0200

    Solution, project, controller, basic routes.

---
+++ src/ZeeBi.UI/Controllers/UIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZeeBi.UI.Controllers
+{
+    public class UIController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Redirect(string id)
+		{
+			return new RedirectResult("http://www.google.com?q=" + id, false);
+		}
+    }
+}
---
+++ src/ZeeBi.UI/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ZeeBi.UI
+{
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
+
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				"Redirect", // Route name
+				"{id}", // URL with parameters
+				new { controller = "UI", action = "Redirect" } // Parameter defaults
+			);
+
+			routes.MapRoute(
+				"Default", // Route name
+				"{controller}/{action}", // URL with parameters
+				new { controller = "UI", action = "Index" } // Parameter defaults
+			);
+		}
+
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+
+			RegisterGlobalFilters(GlobalFilters.Filters);
+			RegisterRoutes(RouteTable.Routes);
+		}
+	}
+}

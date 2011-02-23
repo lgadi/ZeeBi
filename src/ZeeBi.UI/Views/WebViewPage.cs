216cc3ba43af921fdda44ec9724ed50e32672438
Avish <avishay.lavie@gmail.com>
2011-02-23 19:00:12 +0200

    More merge + fixed FullRoot usages.

---
+++ src/ZeeBi.UI/Views/WebViewPage.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace ZeeBi.UI.Views
+{
+	public abstract class WebViewPage<T> : System.Web.Mvc.WebViewPage<T>
+	{
+		public static string FullRoot
+		{
+			get { return "http://" + HttpContext.Current.Request.Url.Authority; }
+		}
+	}
+}

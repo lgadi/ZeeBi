b7fa0a6ca2f4243d7b835bf9ac00e50bf5f73fb7
Gadi Lifshitz <gadilif@gmail.com>
2011-02-24 17:06:20 +0200

    adding openid support

---
+++ src/ZeeBi.UI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace ZeeBi.UI.Models
+{
+	public class User
+	{
+		public ObjectId Id { get; set; }
+		public string UserName { get; set; }
+		public string OpenId { get; set; }
+		public string Friendly { get; set; }
+		public string Email { get; set; }
+	}
+}

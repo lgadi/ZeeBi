b8ed8691a1502ee0582dc493e1e046ad2555e68a
Avish <avishay.lavie@gmail.com>
2011-02-26 23:28:25 +0200

    version bump for UI redesign

--- src/ZeeBi.UI/Properties/AssemblyInfo.cs
+++ src/ZeeBi.UI/Properties/AssemblyInfo.cs
@@ -3,12 +3,12 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZeeBi.UI")]
-[assembly: AssemblyCompany("Delver dojo")]
-[assembly: AssemblyProduct("ZeeBi.UI")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTitle("ExtremelyLongUrlShorteningServiceProvider.info")]
+[assembly: AssemblyCompany("Delver Dojo")]
+[assembly: AssemblyProduct("ExtremelyLongUrlShorteningServiceProvider.info")]
+[assembly: AssemblyCopyright("Copyright © SHC IL 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
